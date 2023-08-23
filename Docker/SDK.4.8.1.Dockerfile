b88ffbd8ec1c6ae16f9e8bfb225d322e11e1eb46
Behzad Behboodi <b.behboodi@elitechgroup.com>
2023-08-23 17:08:56 +0200

    Changes

--- Docker/SDK.4.8.1.Dockerfile
+++ Docker/SDK.4.8.1.Dockerfile
@@ -1,4 +1,3 @@
 FROM mcr.microsoft.com/dotnet/framework/sdk:4.8.1
 ENV DEV_HUB=C:/ELITech/DevHub
 COPY ${WORKSPACE}/Scripts ${DEV_HUB}/Scripts
-RUN [ "CMD" ]
--- Scripts/BuildSolution.ps1
+++ Scripts/BuildSolution.ps1
@@ -1,17 +1,8 @@
 param(
     [Parameter(Mandatory)]
-    [String]
     $SolutionPath,
-
-    [Parameter]
-    [String]
     $Target = "Rebuild",
-
-    [Parameter]
-    [String]
     $Configuration = "Release"
-
-    
 )
 
 dotnet msbuild $SolutionPath -t:$Target -p:Configuration=$Configuration
